08ae5d13ef957e431f76124b30edc14e357c5ae9
ShabanaD <shaby_1897@hotmail.com>
2017-01-21 16:35:35 -0500

    moneybuns.js is crap ignore it for now

---
+++ moneybuns.js
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+
+<h1>Instructions: </h1>
+
+<p>1. Buy Stocks</p>
+<p>2. Try no to lose all your money!</p>
+<p>3. Be nice!!</p>
+
+<button type= "button">START</button>
+
+</body>
+</html>
