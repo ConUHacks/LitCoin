56df8fb8d8189373f05d6ed1afe78bc967cde70b
luy89 <luy89@mcmaster.ca>
2017-01-22 00:22:54 -0500

    adding js for top bar

---
+++ stuff/Scripts/scroll.js
@@ -0,0 +1,3 @@
+for (var i = 0; i < 12; i++) {
+    document.write(listOfStocks[i].retName() + " " + listOfStocks[i].retChange() + "\t");
+}
--- stuff/Scripts/stocks.js
+++ stuff/Scripts/stocks.js
@@ -1,17 +1,35 @@
-function stock(name, cost) {
+function stock(name, cost, change) {
 
     this.name = name;
     this.cost = cost;
+    this.change = change;
 
     this.retName = function(this) {
         return this.name;
     };
-    this.cost = function(this) {
+    this.retCost = function(this) {
         return this.cost;
+    };
+    this.retChange = function(this) {
+        return this.change;
     };
 
     this.change = function(this, numberOfshares, neg) {
+        this.change = neg * this.cost * numberOfshares;
         this.cost += neg * this.cost * numberOfshares;
     };
 
 }
+
+var listOfStocks = [
+    new stock("Google", 10),
+    new stock("Yellow Pages", 10),
+    new stock("SAP", 10),
+    new stock("Vigilant: A DRW Company", 10),
+    new stock("Nuance", 10),
+    new stock("Spiria", 10),
+    new stock("Morgan Stanley", 10),
+    new stock("Dr. Constantinides", 10),
+    new stock("CAE", 10),
+    new stock("Microsoft", 10)
+];
