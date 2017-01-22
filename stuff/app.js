220958ee6686dd85b5f2b649451c5cecba37c8d6
luy89 <luy89@mcmaster.ca>
2017-01-22 00:22:34 -0500

    dunno

--- stuff/app.js
+++ stuff/app.js
@@ -1,39 +1,37 @@
-jQuery(function($){    
+jQuery(function($) {
     'use strict';
-// taken from http://socket.io/docs/
-var numPlayers = 0
-fucntion() {
-    var IO {
-        
+    // taken from http://socket.io/docs/
+    var numPlayers = 0
+    var IO = function() {
+
         //socket.io connection code
         bounds: 0,
-        trackLength: 0,
-            
-        init: function() {
+        trackLength: 0
+
+            init: function() {
             IO.socket = io.connect();
             IO.bindEvents();
         },
-            
+
         bindEvents: function() {
             // checks if connected
             IO.socket.on('connected', IO.onConnected);
             // checks nummber of players
             IO.socket.on('playersOn', IO.updateNumPlayers);
         },
-            
+
         updateNumPlayers: function(numPlayersReceived) {
             numPlayers = numPlayersReceived;
         },
-            
-            
-        onConnected: function(bounds, trackLength) {
-            
-            IO.bounds = bounds;
-            IO.tracklength = trackLength; 
-            IO.socket.emit('lesgo hoes');
-        };
-    }
-};
 
-IO.init();
-} ($));
+
+        onConnected: function(bounds, trackLength) {
+
+            IO.bounds = bounds;
+            IO.tracklength = trackLength;
+            IO.socket.emit('lesgo hoes');
+        }
+    };
+
+    IO.init();
+}($));
