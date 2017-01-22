8b528378550e69bf6411062a9f3855f0570d4b98
luy89 <luy89@mcmaster.ca>
2017-01-22 00:48:25 -0500

    please test

--- stuff/Scripts/scroll.js
+++ stuff/Scripts/scroll.js
@@ -1,3 +1,3 @@
 for (var i = 0; i < 12; i++) {
-    document.write(listOfStocks[i].retName() + " " + listOfStocks[i].retChange() + "\t");
+    $('<h4>').html(listOfStocks[i].retName() + ' ' + listOfStocks[i].retChange() + '\t')
 }
